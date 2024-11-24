3608e60f578f91e3bb964d4d95747a80dde832cf
Kelechi Odom <kelechiodom10@gmail.com>
2024-11-24 13:57:04 +0000

    feat: add custom button to test styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button
+  class="bg-primary text-primary-foreground hover:shadow-offset-sm ring-offset-background focus-visible:ring-primary m-4 inline-flex w-fit items-center justify-center rounded-sm px-5 py-1.5 text-sm font-normal transition-all duration-200 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none active:translate-x-1 active:translate-y-1 active:shadow-none"
+>
+  Login
+</button>
