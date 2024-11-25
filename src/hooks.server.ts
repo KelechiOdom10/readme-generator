11a23e90c0cd655ae0edf85d429c761aa56cf398
Kelechi Odom <kelechiodom10@gmail.com>
2024-11-25 19:12:27 +0000

    feat: add server-side hook for font preloading

---
+++ src/hooks.server.ts
@@ -0,0 +1,13 @@
+import type { Handle } from "@sveltejs/kit";
+import { sequence } from "@sveltejs/kit/hooks";
+
+const preloadFonts: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event, {
+    preload: ({ type }) => {
+      return type === "font";
+    }
+  });
+  return response;
+};
+
+export const handle = sequence(preloadFonts);
