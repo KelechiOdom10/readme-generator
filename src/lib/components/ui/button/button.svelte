65587017515c027751887887e9000d720a0bd7a3
Kelechi Odom <kelechiodom10@gmail.com>
2025-01-05 17:32:27 +0100

    style: change font weight to medium

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -5,7 +5,7 @@
 
   export const buttonVariants = tv({
     base: [
-      "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-normal",
+      "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium",
       "ring-offset-background focus-visible:outline-none focus-visible:ring focus-visible:ring-ring focus-visible:ring-offset-2",
       "transition-all duration-200 cursor-pointer",
       "disabled:pointer-events-none disabled:opacity-50",
@@ -23,13 +23,19 @@
           "bg-destructive text-destructive-foreground",
           "hover:bg-destructive/90",
           "focus-visible:ring-destructive"
+        ],
+        ghost: ["bg-ghost text-ghost-foreground", "hover:bg-ghost/90", "focus-visible:ring-ghost"],
+        outline: [
+          "border border-input bg-background",
+          "hover:bg-accent hover:text-accent-foreground",
+          "focus-visible:ring-ring"
         ]
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10"
+        icon: "h-9 w-9"
       }
     },
     defaultVariants: {
@@ -45,11 +51,13 @@
     WithElementRef<HTMLAnchorAttributes> & {
       variant?: ButtonVariant;
       size?: ButtonSize;
+      class?: ClassNameValue;
     };
 </script>
 
 <script lang="ts">
   import { cn } from "$lib/utils.js";
+  import type { ClassNameValue } from "tailwind-merge";
 
   let {
     class: className,
