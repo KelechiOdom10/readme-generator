020e44ab5e6b676edacc27c8df3ef2139ae4e198
Kelechi Odom <kelechiodom10@gmail.com>
2025-01-05 16:54:23 +0100

    feat: add tabs component used to toggle between editor and preview in mobile view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,17 +3,22 @@
   import MarkdownEditor from "$lib/components/editor/markdown-editor.svelte";
   import MarkdownPreview from "$lib/components/preview/markdown-preview.svelte";
   import Header from "$lib/components/Header.svelte";
+  import * as Tabs from "$lib/components/ui/tabs/index";
 
-  let isPreviewMode = false;
+  let activeView = $state<"editor" | "preview">("editor");
 </script>
 
 <main class="flex min-h-screen flex-col">
   <Header />
   <div class="container mx-auto flex flex-col gap-4 p-4">
+      <Tabs.Root bind:value={activeView} class="w-[400px] md:hidden">
+        <Tabs.List>
+          <Tabs.Trigger value="editor">Editor</Tabs.Trigger>
+          <Tabs.Trigger value="preview">Preview</Tabs.Trigger>
+        </Tabs.List>
+      </Tabs.Root>
     <div class="grid w-full flex-1 gap-8 md:grid-cols-2">
-      <!-- Editor -->
-      <div class={isPreviewMode ? "hidden md:block" : ""}>
-        <!-- <h2 class="mb-4 text-xl font-semibold">Editor</h2> -->
+      <div class={activeView === "preview" ? "hidden md:block" : "block"}>
         {#if readmeStore.currentSection}
           <div>
             <MarkdownEditor
@@ -25,7 +30,7 @@
       </div>
 
       <!-- Preview -->
-      <div class={!isPreviewMode ? "hidden md:block" : ""}>
+      <div class={activeView === "editor" ? "hidden md:block" : "block"}>
         <MarkdownPreview bind:value={readmeStore.preview} />
       </div>
     </div>
