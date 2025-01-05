26304b1c9748566b439e9e5b9891e17d217e1938
Kelechi Odom <kelechiodom10@gmail.com>
2025-01-05 12:50:24 +0100

    feat: add a preview section for showing what full md looks like

---
+++ src/lib/components/preview/markdown-preview.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import CartaEditor from "../editor/carta-editor.svelte";
+
+  let { value = $bindable() } = $props();
+</script>
+
+<div
+  class="
+  prose prose-zinc dark:prose-invert
+  prose-headings:text-foreground prose-p:text-secondary-foreground prose-a:text-blue-400
+  prose-code:text-foreground prose-pre:text-foreground prose-pre:bg-gray-900/95! dark:prose-pre:bg-secondary!
+  prose-ul:text-secondary-foreground
+  prose-li:text-secondary-foreground
+  mt-11 h-full w-full"
+>
+  <CartaEditor
+    bind:value
+    labels={{ previewTab: undefined, writeTab: undefined }}
+    selectedTab="preview"
+    mode="tabs"
+    disableToolbar
+  />
+</div>
