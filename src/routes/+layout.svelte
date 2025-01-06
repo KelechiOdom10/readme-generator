ce6d5934ff60c7d839174e9d3aeb94391d81f90c
Kelechi Odom <kelechiodom10@gmail.com>
2025-01-06 15:37:09 +0100

    feat: add SEO meta tags in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,22 @@
   let { children } = $props();
 </script>
 
+<svelte:head>
+  <title>README Generator</title>
+  <meta
+    name="description"
+    content="Use Readmegen's editor to create a README. It is the easiest way to create a README for your projects"
+  />
+  <meta
+    name="keywords"
+    content="readme, generator, readme generator, easiest, create, markdown, git, github, gitlab"
+  />
+  <meta property="og:title" content="README Generator" />
+  <meta property="og:description" content="The easiest way to create a README" />
+  <meta property="og:image" content="https://readmegen.pages.dev/logo.png" />
+  <meta property="og:url" content="https://readmegen.pages.dev" />
+</svelte:head>
+
 <Sidebar.Provider>
   <AppSidebar />
   <main>
